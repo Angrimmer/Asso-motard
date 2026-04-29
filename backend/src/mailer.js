3de8998f28f12dd6da02772fb2745cb6d94fac6b
Alex <alex67470@gmail.com>
2026-04-29 10:40:33 +0200

    mailer(hot) get the new workmail

--- backend/src/mailer.js
+++ backend/src/mailer.js
@@ -6,8 +6,8 @@ const transporter = nodemailer.createTransport({
   port: process.env.MAIL_PORT,
   secure: false, // true si port 465, false pour 587
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
   },
 });
 
